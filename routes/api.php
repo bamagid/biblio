789d4a53c4dda7c533df893207f0a391c15830b9
bamagid <81322603+bamagid@users.noreply.github.com>
2024-08-07 08:25:51 +0000

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -11,8 +11,10 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 Route::delete('livres/emptyTrashes', [LivreController::class, "emptyTrashes"])->middleware("auth");
 Route::get('livres/Trashed', [LivreController::class, "Trashed"])->middleware("auth");
+Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 Route::apiResource("livres", LivreController::class)->only('index', 'show');
+Route::apiResource('categories', CategorieController::class)->except('store','update','destroy');
 Route::middleware("auth")->group(
     function () {
         Route::get('logout', [AuthController::class, 'logout']);
@@ -24,5 +26,20 @@ Route::middleware("auth")->group(
         // Route::delete('livres/emptyTrashes', [LivreController::class, "emptyTrashes"]);
         // Route::get('livres/Trashed', [LivreController::class, "Trashed"]);
         Route::apiResource('emprunts', EmpruntController::class)->except('destroy');
+
+
+    /********************* Gestion Categories  **************************/
+
+    // Routes pour le contrôleur CategorieController
+    Route::apiResource('categories', CategorieController::class)->except('index','show');
+
+    // Route pour restaurer une catégorie supprimée
+    Route::post('categories/{id}/restore', [CategorieController::class, 'restore']);
+
+    // Route pour supprimer définitivement une catégorie
+    Route::delete('categories/{id}/force-delete', [CategorieController::class, 'forceDelete']);
+
+    // Route pour récupérer les catégories supprimées
+    Route::get('categories/trashed', [CategorieController::class, 'Trashed'])
     }
 );
